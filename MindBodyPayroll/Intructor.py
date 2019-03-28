dca2a807dd41588712730be394a552c4b4df493c
Marshall Powell <marshallpowell@Marshalls-MacBook-Air.local>
2019-03-27 23:35:54 -0700

    instructor class, basic pandas

---
+++ MindBodyPayroll/Intructor.py
@@ -0,0 +1,4 @@
+class Instructor:
+    def __init__(self, name):
+        self.name = name
+        self.studentEntries= []
--- MindBodyPayroll/MindBodyPayroll.py
+++ MindBodyPayroll/MindBodyPayroll.py
@@ -1,1 +1,10 @@
-print("Beginning MindBody Payroll")
+import numbers
+import pandas as pandas
+from StudentEntry import StudentEntry
+
+print("\n------------------------------\n\nBeginning MindBody Payroll\n\n------------------------------\n\n\n")
+h = StudentEntry()
+print(h.r)
+print('\n\n')
+df = pandas.read_csv('/Users/marshallpowell/Desktop/For Marshall - Sheet1.csv')
+print(df.head())
