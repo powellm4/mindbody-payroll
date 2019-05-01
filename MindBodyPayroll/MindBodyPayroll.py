5bfbfa4517198299393f0284853c6d74195ede64
Marshall Powell <powellmarshall3@gmail.com>
2019-04-30 23:23:41 -0700

    make instructor classes free if given by jamal or carolina

--- MindBodyPayroll/MindBodyPayroll.py
+++ MindBodyPayroll/MindBodyPayroll.py
@@ -18,18 +18,18 @@ pd.set_option('display.width', 1500)
 # get list of files from processedData folder
 list_of_processed_files = [name for name in os.listdir(input_folder) if "01-Class" in name]
 
-# create folder for instructor exports
+# create folders for outputs
 create_folder(public_classes_folder)
 create_folder(instructor_dance_folder)
 create_folder(totals_folder)
 
 
 # dataFrame for pricing options lookup
-po_df = pd.read_csv(special_rates_path)
+po_df = pd.read_csv(pricing_options_path)
 po_df = format_column_headers(po_df)
 po_df = po_df.set_index('Pricing_Option')
 
-# shortlist = [list_of_processed_files[8]]#, list_of_processed_files[9], list_of_processed_files[10]]
+# test_list = [list_of_processed_files[8]]#, list_of_processed_files[9], list_of_processed_files[10]]
 
 # for each file in dataProcessing/dat/ folder
 print("\nWriting individual instructor CSVs\n----------")
