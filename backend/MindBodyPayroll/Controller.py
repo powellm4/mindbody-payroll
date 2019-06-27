65490b2925ef52b7ec241c931b6ac9f5e699f739
Nuatu Tseggai <nuatu@Nuatus-MacBook-Pro.local>
2019-06-27 11:30:00 -0700

    WIP webserver configs

--- backend/MindBodyPayroll/Controller.py
+++ backend/MindBodyPayroll/Controller.py
@@ -11,8 +11,16 @@ import pandas as pd
 from constants import *
 from forms import AppendForm
 import urllib.request
-from app import app
+#from app import app
 from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = './uploads/'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = set(['xls'])
 
