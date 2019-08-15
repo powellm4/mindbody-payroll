d0aecb45495cbcb977437658e930cb3f89795021
marshallpowell <powellmarshall3@gmail.com>
2019-08-15 15:27:34 -0700

    stash

--- backend/dataProcessing/bin/s00-generate_csv_files.sh
+++ backend/dataProcessing/bin/s00-generate_csv_files.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Read an input filename (raw) and generate many csv files
-# source ./bin/s00-generate_csv_files.sh $fileName
+# source ./bin/s00-generate_csv_files.sh <a_fileName>
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 fileName="$1"
-#fileName='./raw/Payroll Report 1-1-2019 - 1-31-2019.xls'
-#fileName='./raw/Payroll_Report_4-1-2019_4-14-2019.xls'
-#fileName='./raw/Payroll Report 5-1-2019 - 5-15-2019.xls'
-#fileName='./raw/Payroll Report 6-1-2019 - 6-15-2019.xls'
-#fileName='./raw/Payroll_Report_6-1-2019-6-15-2019.xls'
-
+#-
+#fileName='./raw/Payroll Report 7-16-2019 - 7-31-2019.xls'
+#fileName='./raw/Payroll Report 7-1-2019 - 7-7-2019.xls'
+#fileName='./raw/Payroll Report 7-1-2019 - 7-14-2019.xls'
+#-
 # echo "fileName: \"$fileName\""
-# rm -f ./dat/* ./tmp/* ./log/*
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rm -f ./dat/* ./tmp/* ./log/*
 cp -a "$fileName" ./tmp/s00-01All.txt
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Convert tabs to spaces and Convert html symbols to text
 source ./bin/s01-initial_process.sh \
        ./tmp/s00-01All.txt
@@ -21,30 +25,35 @@ source ./bin/s01-initial_process.sh \
        #Output: ./tmp/s01-01All.txt
        #Output: ./tmp/s01-02All.txt
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Do initial cleaning and flag the significat lines
 source ./bin/s02-00flag_the_significant_lines.sh \
        ./tmp/s01-02All.txt
        #Input-: ./tmp/s01-02All.txt
        #Output: ./tmp/s02-01flagSignificantLines.txt
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Insert instructor names for each record
 source ./bin/s03-00insert_instructor_names.sh \
        ./tmp/s02-01flagSignificantLines.txt
        #Input-: ./tmp/s02-01flagSignificantLines.txt
        #Output: ./tmp/s03-01insertedNames.txt
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Create a csv file for Regular Classes
 source ./bin/s04-00Create_csv_file_for_Regular_Classes.sh \
        ./tmp/s03-01insertedNames.txt
        #Input-: ./tmp/s03-01insertedNames.txt
        #Output: ./dat/00-01-Class-All.csv
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Create a csv file for Private Lessons
 source ./bin/s05-00Create_csv_file_for_Private_Lessons.sh \
        ./tmp/s03-01insertedNames.txt
        #Input-: ./tmp/s03-01insertedNames.txt
        #Output: ./dat/00-02-Private-All.csv
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Generate one csv file for each set of instructors for Regula Classes
 source ./bin/s06-00generate_csv_files_for_Regular_Classes.sh \
        ./tmp/s04-01ClassAll.txt
@@ -53,6 +62,7 @@ source ./bin/s06-00generate_csv_files_for_Regular_Classes.sh \
        #Output: ./dat/01-Class-002-<instructorName>.txt
        #Output: ./dat/01-Class-...-<instructorName>.txt
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # generate one csv file for each set of instructors for Private Lessons:
 source ./bin/s07-00generate_csv_files_for_Private_Lessons.sh \
        ./tmp/s05-01PrivateAll.txt
--- backend/dataProcessing/bin/s01-initial_process.sh
+++ backend/dataProcessing/bin/s01-initial_process.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Convert tabs to spaces and Convert html symbols to text
 # source ./bin/s01-initial_process.sh \
 #        ./tmp/s00-01All.txt 
@@ -6,9 +8,11 @@
 #        #Output: ,/tmp/s01-01All.txt
 #        #Output: ./tmp/s01-02All.txt
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 1) Convert tabs to spaces:
 expand $1 > ./tmp/s01-01All.txt
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 2) Convert html symbols to text:
 cat ./tmp/s01-01All.txt | sed 's/\xC3\xB1/n/g' \
                     | sed 's/\&#241;/n/g'  \
