8adfab27d7feff78f05ad38a12c33d18bac49c10
Marshall Powell <marshall.powell@altsourcesoftware.com>
2020-11-04 08:01:14 -0800

    update gitignore -- remove db and secrets folders

--- backend/src/config.py
+++ backend/src/config.py
@@ -20,7 +20,7 @@ quickbooks_redirect_uri = "http://localhost:56733/oauth-redirect"
 quickbooks_oauth2_base_uri = "https://appcenter.intuit.com/connect/oauth2"
 
 # Google Cloud Storage
-google_storage_cred_file = 'vmac-1-6a2f92d95ce4.json'
+google_storage_cred_file = 'secrets/vmac-1-6a2f92d95ce4.json'
 google_bucket_name = "vmac-mindbody-data"
 prices_blob_name = "Prices/pricing-options.csv"
 classes_blob_name = "Classes/class-name-lookup.csv"
